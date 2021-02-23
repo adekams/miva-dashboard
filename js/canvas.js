c83f7190c35b5ed138e9d526bbdfe7aae3dde4df
AdenikeSalau <kifademi@gmail.com>
2021-02-23 18:42:38 +0100

    dashboard

---
+++ js/canvas.js
@@ -0,0 +1,111 @@
+var ctxA = document.getElementById('barChart').getContext('2d');
+var ctxB = document.getElementById('barChart').getContext('2d');
+
+var laptopBackground = ctxA.createLinearGradient(0, 0, 0, 200);
+var mobileBackground = ctxA.createLinearGradient(0, 0, 0, 200);
+laptopBackground.addColorStop(0, 'rgba(255, 199, 118, 0.4)');   
+laptopBackground.addColorStop(1, 'rgba(254, 172, 134, 1)');
+mobileBackground.addColorStop(0, 'rgba(218, 67, 163, 0.4)');   
+mobileBackground.addColorStop(1, 'rgba(175, 83, 179, 1)');
+
+
+// bar chart
+var barChart =  new Chart(ctxA, {
+    type: 'bar',
+    data: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8],
+        datasets: [
+            {
+                label: "Laptop",
+                data: [40, 30, 60, 35, 60, 25, 50, 40],
+                backgroundColor: laptopBackground,
+                borderWidth: 0
+            },
+            {
+                label: "Mobile",
+                data: [50, 60, 40, 70, 35, 75, 30, 0],
+                backgroundColor: mobileBackground,
+                borderWidth: 0
+            },
+        ]
+    },
+
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Top Selling Categories'
+            },
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: true,
+            position: 'top',
+            labels: {
+            boxWidth: 20,
+            fontColor: 'black'
+            }
+        },
+    }
+});
+
+
+// line chart
+var config = {
+    type: 'line',
+    data: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8],
+        datasets: [{
+            label: 'Apple',
+            data: [0, 30, 60, 25, 60, 25, 50, 0],
+            backgroundColor: 'rgba(23, 162, 184, .6)',
+            borderColor: 'rgba(23, 162, 184, .6)',
+            borderWidth: 2,
+            pointRadius: 2,
+            pointHitRadius: 40,
+        },
+        {  
+            label: 'Samsung',
+            data: [0, 60, 25, 80, 35, 75, 30, 0],
+            backgroundColor: 'rgba(232, 62, 140, .6)',
+            borderColor: 'rgba(232, 62, 140, .6)',
+            borderWidth: 2,
+            pointRadius: 2,
+            pointHitRadius: 40,
+        },
+    ],
+        fill: true,
+    },
+
+       
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Product Sales'
+            },
+        },
+        legend: {
+            display: true,
+            position: 'top',    
+            labels: {
+              boxWidth: 20,
+              fontColor: 'black',
+            }
+        },
+    }
+};
+
+window.onload = function() {
+    var ctx = document.getElementById('lineChart').getContext('2d');
+    window.myLine = new Chart(ctx, config);
+    // window.myLine = new Chart(ctxA, barChart);
+};
---
+++ js/script.js
@@ -0,0 +1,56 @@
+// const navMenu = document.getElementsByClassName("nav-menu")
+
+const buttons = document.querySelectorAll("button")
+const navMenu = document.querySelectorAll(".nav-menu")
+const navList = document.querySelectorAll(".menu-item")
+const hamburger = document.querySelector(".hamburger i")
+const sidebar = document.querySelector("aside")
+const wrapper = document.querySelector(".wrapper")
+const headerNav = document.querySelector("header nav")
+
+// toggle sidebar
+hamburger.addEventListener('click', (e) => {
+    console.log("clicked")
+    
+    sidebar.classList.toggle('active')
+    wrapper.classList.toggle('m-left')
+    headerNav.classList.toggle('m-left')
+    sidebar.classList.toggle("high")
+
+    // change toggle icon to bars or close on click
+    if(e.target.classList.contains('fa-times')) {
+        e.target.classList.remove('fa-times')
+        e.target.classList.add('fa-bars')
+    } 
+    else if(e.target.classList.contains('fa-bars')){
+        e.target.classList.remove('fa-bars')
+        e.target.classList.add('fa-times')
+    }
+})
+
+
+
+// toggle side nav menu
+buttons.forEach((button) => {
+    let currentMenu = button
+    currentMenu.addEventListener("click", (e) => {
+         console.log("toggle")
+         
+        navList.forEach((list) => {
+            // console.log(e.target, list.previousElementSibling)
+            // list.classList.remove("active")
+            // button.classList.remove("btn-down")
+            button.classList.toggle("btn-clicked")
+            
+            if (list.previousElementSibling === e.target) {
+                list.classList.toggle("active")
+            }
+        })
+        button.classList.toggle("btn-down")
+        button.classList.toggle("btn-clicked")
+    })
+
+    
+})
+
+
